5ddeb34c9a9133942d6c4e67c407eb981255729e
Dhonch <webdevpl@i.ua>
2017-01-20 21:52:40 +0300

    added js file

--- pink-donut/scripts/script.js
+++ pink-donut/scripts/script.js
@@ -1,8 +1,44 @@
+
+
 window.onscroll = function() {
   var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-  if ( scrolled > 100 ) {
-  	 document.getElementByClass('scrollblock');
-  	 el.classList.add("bg-for-scrollblock");
+  var elem = document.querySelector(".scrollblock");
+  if (  scrolled <= 900  ){
+  	 elem.classList.add("bg-for-scrollblock");} 
+  	 else {
+  		 elem.classList.remove("bg-for-scrollblock");
+  	} 
 
-  }
-}
+
+  var elem = document.querySelector(".scrollblockjs2");
+  if ( scrolled >= 1000  & scrolled < 1300 ){
+  	 elem.classList.add("bg-for-scrollblock");
+  		} else if ( scrolled >= 1400 ){
+  			 elem.classList.remove("bg-for-scrollblock");
+  		} 
+		  	else  {
+		  		 elem.classList.remove("bg-for-scrollblock");
+		  	} 
+
+
+  var elem = document.querySelector(".scrollblockjs3");
+  if ( scrolled >= 1400 & scrolled <= 1600 ){
+  	 elem.classList.add("bg-for-scrollblock");
+  		} else if ( scrolled <= 1600 ){
+  			 elem.classList.remove("bg-for-scrollblock");
+  		} 
+		  	else  {
+		  		 elem.classList.remove("bg-for-scrollblock");
+		  	} 
+
+  var elem = document.querySelector(".scrollblockjs4");
+  if ( scrolled >= 1800  ){
+  	 elem.classList.add("bg-for-scrollblock");
+  		} else if ( scrolled <= 2300 ){
+  			 elem.classList.remove("bg-for-scrollblock");
+  		} 
+		  	else  {
+		  		 elem.classList.remove("bg-for-scrollblock");
+		  	} 				  
+};
+	
