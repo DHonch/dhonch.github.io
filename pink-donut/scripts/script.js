25e06e4931821339180d9d8d95e8a151b844a0c0
Dhonch <webdevpl@i.ua>
2017-01-25 19:16:01 +0300

    added new function

--- pink-donut/scripts/script.js
+++ pink-donut/scripts/script.js
@@ -1,6 +1,6 @@
 
 
-	
+	/*
 window.onscroll = function() {
   var scrolled = window.pageYOffset || document.documentElement.scrollTop;
   var elem = document.querySelector(".scrollblock");
@@ -21,4 +21,31 @@ window.onscroll = function() {
 		  		 elem.classList.remove("bg-for-scrollblock");
 		  	} 				  
 };
-	
+	*/
+
+window.onscroll = function() {
+  var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+  var elem1 = document.querySelector(".ido");
+  if (  scrolled >500  ){
+     elem1.style.marginTop = "0";
+     elem1.style.transitionDuration = "1.3s";
+     elem1.style.opacity = "1";} 
+     else {
+    };
+  var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+  var elem2 = document.querySelector(".works");
+  if (  scrolled >1100  ){
+     elem2.style.marginTop = "0";
+     elem2.style.transitionDuration = "1.3s";
+     elem2.style.opacity = "1";} 
+     else {
+    } ;
+  var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+  var elem2 = document.querySelector(".about");
+  if (  scrolled >2300  ){
+     elem2.style.marginTop = "0";
+     elem2.style.transitionDuration = "1.3s";
+     elem2.style.opacity = "1";} 
+     else {
+    } ;
+}
