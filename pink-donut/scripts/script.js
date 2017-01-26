e8cda47f823b8d4a944e7309f3aec65e4868ad44
Dhonch <webdevpl@i.ua>
2017-01-26 12:57:04 +0300

    added loader page

--- pink-donut/scripts/script.js
+++ pink-donut/scripts/script.js
@@ -1,51 +1,52 @@
 
-
-	/*
-window.onscroll = function() {
+ window.onscroll = function() {
   var scrolled = window.pageYOffset || document.documentElement.scrollTop;
   var elem = document.querySelector(".scrollblock");
-  if (  scrolled <= 900  ){
+  var elem0 = document.querySelector(".scrollblockjs2");
+  var elem1 = document.querySelector(".ido");
+  var elem2 = document.querySelector(".works");
+  var elem3 = document.querySelector(".about") ;
+  var head = document.querySelector(".header"); 
+  var h = head.clientHeight; 
+  var menu = document.getElementById('menu')
+
+  if (  scrolled > 0*h & scrolled < 0.6*h  ){
   	 elem.classList.add("bg-for-scrollblock");} 
-  	 else {
+  	 else if (scrolled > 0.6*h ){
   		 elem.classList.remove("bg-for-scrollblock");
-  	} 
+  	}
+    else {
 
+    } ;
 
-  var elem = document.querySelector(".scrollblockjs2");
-  if ( scrolled >= 1000  & scrolled < 1300 ){
-  	 elem.classList.add("bg-for-scrollblock");
+  if ( scrolled > 0.6*h  & scrolled < 1.5*h ){
+  	 elem0.classList.add("bg-for-scrollblock");
   		} else if ( scrolled >= 1400 ){
-  			 elem.classList.remove("bg-for-scrollblock");
+  			 elem0.classList.remove("bg-for-scrollblock");
   		} 
 		  	else  {
-		  		 elem.classList.remove("bg-for-scrollblock");
-		  	} 				  
-};
-	*/
+		  		 elem0.classList.remove("bg-for-scrollblock");
+		  	} ;
 
-window.onscroll = function() {
-  var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-  var elem1 = document.querySelector(".ido");
-  if (  scrolled >500  ){
+  if (  scrolled > 0.3*h  ){
      elem1.style.marginTop = "0";
      elem1.style.transitionDuration = "1.3s";
      elem1.style.opacity = "1";} 
      else {
     };
-  var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-  var elem2 = document.querySelector(".works");
-  if (  scrolled >1100  ){
+
+  if (  scrolled > 1100  ){
      elem2.style.marginTop = "0";
      elem2.style.transitionDuration = "1.3s";
      elem2.style.opacity = "1";} 
      else {
     } ;
-  var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-  var elem2 = document.querySelector(".about");
-  if (  scrolled >2300  ){
-     elem2.style.marginTop = "0";
-     elem2.style.transitionDuration = "1.3s";
-     elem2.style.opacity = "1";} 
+
+  if (  scrolled > 2300  ){
+     elem3.style.marginTop = "0";
+     elem3.style.transitionDuration = "1.3s";
+     elem3.style.opacity = "1";} 
      else {
-    } ;
-}
+    } ;				  
+};
+	
